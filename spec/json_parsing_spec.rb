64c639d57164ee6a59e292d4cf64ca4dc623182a
Tech-AYY <tech-ayy@Tech-AYYs-MacBook-Air.local>
2018-03-20 22:49:09 +0000

    3 tests complete

--- spec/json_parsing_spec.rb
+++ spec/json_parsing_spec.rb
@@ -17,5 +17,19 @@ describe CustomerInfo do
 
   end
 
+  it "Last person in array should be called Rozalin Adamiec" do
+    expect(@file.json_customer_file.last["first_name"]).to eq("Rozalin")
+    expect(@file.json_customer_file.last["last_name"]).to eq("Adamiec")
+  end
+
+  it "All phone numbers are less than 10 digits" do
+    @file.json_customer_file.each do |phone|
+      expect(phone["Phone_number"]["mobile"].first.length).to eq(10)
+      expect(phone["Phone_number"]["mobile"].last.length).to eq(10)
+
+    end
+  end
+
+
 
 end
